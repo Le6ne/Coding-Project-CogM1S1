24803fe503116321b81979023e85f41de3314593
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-11-05 10:43:24 +0100

    quick fixes and added stimulis

--- script/constants.py
+++ script/constants.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math as m
+from expyriment.misc.constants import C_WHITE, C_BLACK
 
 """
 General constants
@@ -7,32 +8,34 @@ General constants
 
 NUM_OF_DOT = 8
 
-EXP_RADIUS = 100
+EXP_RADIUS = 150
 
 DOT_RADIUS = 10
+DOT_COLOR = "grey"
 
 TRIGGERED_DOT_RADIUS = 15
+TRIGGERED_DOT_COLOR = "yellow"
 
 """
 Experiment type
 """
-sequences_ez = np.array([([i for i in range(NUM_OF_DOT)], "repeat cw"), 
-                         ([NUM_OF_DOT-i-1 for i in range(NUM_OF_DOT)], "repeat ccw")])
+sequences_ez = [([i for i in range(NUM_OF_DOT)], "repeat cw"),
+                ([NUM_OF_DOT-i-1 for i in range(NUM_OF_DOT)], "repeat ccw")]
 print(sequences_ez)
 
-sequences_cw = np.asarray([([0,2,4,6,1,3,5,7], "2squares cw"), 
-                           ([0,6,4,2,1,7,5,3], "2squares ccw"), 
-                           ([0,2,1,3,2,4,3,5,4,6,5,7,6,0,7,1], "alternate cw"), 
-                           ([0,6,7,5,6,4,5,3,4,2,3,1,2,0,1,6], "alternate ccw"), 
-                           ([0,4,1,5,2,6,3,7], "4diagonales cw"), 
-                           ([0,4,7,3,6,2,5,1], "4diagonales ccw"), 
-                           ([1,2,3,4,0,7,6,5], "2arcs cw"), 
-                           ([0,7,6,5,1,2,3,4], "2arcs ccw"), 
-                           ([0,1,7,2,6,3,5,4], "4segments cw"), 
-                           ([1,0,2,7,3,6,4,5], "4segment ccw"), #TODO: 2 autres symetries
-                           ([0,5,4,1,6,3,2,7], "2rectangles"), 
-                           ([0,4,5,1,2,6,7,3], "2crosses") 
-                           ])
+sequences_cw = [([0,2,4,6,1,3,5,7], "2squares cw"), 
+                ([0,6,4,2,1,7,5,3], "2squares ccw"), 
+                ([0,2,1,3,2,4,3,5,4,6,5,7,6,0,7,1], "alternate cw"), 
+                ([0,6,7,5,6,4,5,3,4,2,3,1,2,0,1,6], "alternate ccw"), 
+                ([0,4,1,5,2,6,3,7], "4diagonales cw"), 
+                ([0,4,7,3,6,2,5,1], "4diagonales ccw"), 
+                ([1,2,3,4,0,7,6,5], "2arcs cw"), 
+                ([0,7,6,5,1,2,3,4], "2arcs ccw"), 
+                ([0,1,7,2,6,3,5,4], "4segments cw"), 
+                ([1,0,2,7,3,6,4,5], "4segment ccw"), #TODO: 2 autres symetries
+                ([0,5,4,1,6,3,2,7], "2rectangles"), 
+                ([0,4,5,1,2,6,7,3], "2crosses") 
+                ]
 
 
 """
--- script/main.py
+++ script/main.py
@@ -2,7 +2,7 @@ from expyriment import design, control, stimuli
 from expyriment.misc.constants import C_WHITE, C_BLACK
 from constants import *
 
-exp = design.Experiment(name="test", background_colour=C_WHITE, foreground_colour=C_BLACK)
+exp = design.Experiment(name="test", background_colour=C_BLACK, foreground_colour=C_WHITE)
 exp.add_data_variable_names([])
 
 control.set_develop_mode()
@@ -15,6 +15,14 @@ def load(stims):
 def timed_draw(*stims):
     t0 = exp.clock.time
     exp.screen.clear()
+    for stim in stims:
+        stim.present(clear=False, update=False)
+    exp.screen.update()
+    t1 = exp.clock.time
+    return t1 - t0
+
+def timed_add(*stims): #same as timed draw but without clear
+    t0 = exp.clock.time
     for stim in stims:
         stim.present(clear=False, update=False)
     exp.screen.update()
@@ -39,6 +47,18 @@ control.start(subject_id=1)
 
 control.set_develop_mode()
 control.initialize(exp)
+
+""" Stimuli """
+dots = [stimuli.Circle(radius = DOT_RADIUS, colour = DOT_COLOR, position = p) for p in dot_positions]
+load(dots)
+
+triggered_dots = [stimuli.Circle(radius = TRIGGERED_DOT_RADIUS, colour = TRIGGERED_DOT_COLOR, position = p) for p in dot_positions]
+load(triggered_dots)
+
 """ Experiment """
 
+timed_draw(*dots)
 
+exp.keyboard.wait()
+
+control.end()

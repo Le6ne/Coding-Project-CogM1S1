d2a320bb405531da5ccc295dca01ede93121a2a8
Anna <147484158+AnnaENS@users.noreply.github.com>
2025-11-05 14:26:28 +0100

    mouse function

--- script/mouse_function.py
+++ script/mouse_function.py
@@ -5,7 +5,7 @@ import math as m
 def wait_for_dot_click(correct_dot_idx, dot_positions, radius=DOT_RADIUS):
     mouse = io.Mouse(show_cursor=True)
     while True:
-        button, (x, y) = mouse.wait()
+        _, (x, y), _ = mouse.wait_press()
         clicked_dot = None
         for idx, (dot_x, dot_y) in enumerate(dot_positions):
             dist = m.sqrt((x - dot_x) ** 2 + (y - dot_y) ** 2)

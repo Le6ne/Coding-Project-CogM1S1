5480e3d92e2169731d4a675facc3150a89726987
Le6ne <lllhappy@free.fr>
2025-11-05 13:44:01 +0100

    Add parameters to create_random_seq function
    Modified create_random_seq to accept length and num_dots parameters for flexibility.

--- script/functions.py
+++ script/functions.py
@@ -1,3 +1,13 @@
-def create_random_seq(): 
+def create_random_seq(length=8, num_dots=8):
+    seq = []
+    current = rd.randint(0, num_dots-1)
+    seq.append(current)
+    for _ in range(length-1):
+        next_dot = rd.randint(0, num_dots-1)
+        while next_dot == current:
+            next_dot = rd.randint(0, num_dots-1)
+        seq.append(next_dot)
+        current = next_dot
+    return seq
 
 
